027a33b89e3c59d5540a95d510952d30e6de1f85
Sakshya Sinha <sakshyasinha32@gmail.com>
2026-07-06 02:22:26 -0400

    fix#3

--- service/common/log_handlers.py
+++ service/common/log_handlers.py
@@ -36,4 +36,3 @@ def init_logging(app, logger_name: str):
     for handler in app.logger.handlers:
         handler.setFormatter(formatter)
     app.logger.info("Logging handler established")
-    
--- service/routes.py
+++ service/routes.py
@@ -120,4 +120,3 @@ def reset_counters():
     global COUNTER  # pylint: disable=global-statement
     if app.testing:
         COUNTER = {}
-        
